aa2bfde89c1eca22ca9a53c157034687b16c9c64
catburston <cat1788@gmail.com>
2014-07-09 11:05:43 +0200

    Refactoring moveForward and moveBackward

--- initialkata/js/rover.js
+++ initialkata/js/rover.js
@@ -26,62 +26,37 @@ var Rover = {
     }
   },
   moveForward: function () {
+    this.advance('forward');
+  },
+  moveBackward: function () {
+    this.advance('backward');
+  },
+  advance: function (direction) {
+    direction = (direction === 'forward') ? 1 : -1;
     var formerPosition = [
       this.position[0],
       this.position[1]
     ];
-    switch (this.orientation) {
-      case 'n':
-        this.position[1] = (this.position[1] + 1) % Grid.sizeY;
-      break;
-      case 'e':
-        this.position[0] = (this.position[0] + 1) % Grid.sizeX;
-      break;
-      case 's':
-        this.position[1] = (this.position[1] - 1);
-        if (this.position[1] < 0) {
-          this.position[1] = Grid.sizeY + this.position[1];
-        }
-      break;
-      case 'w':
-        this.position[0] = (this.position[0] - 1);
-        if (this.position[0] < 0) {
-          this.position[0] = Grid.sizeX + this.position[0];
-        }
-      break;
+    var increment = this.increments[this.orientation];
+    this.position = [
+      (this.position[0] + increment[0] * direction) % Grid.sizeX,
+      (this.position[1] + increment[1] * direction) % Grid.sizeY
+    ];
+    if (this.position[0] < 0) {
+      this.position[0] = Grid.sizeX + this.position[0];
+    }
+    if (this.position[1] < 0) {
+      this.position[1] = Grid.sizeY + this.position[1];
     }
     if (Grid.thereIsObstacle(this.position[0], this.position[1])) {
       this.position = formerPosition;
     }
   },
-  moveBackward: function () {
-    var formerPosition = [
-      this.position[0],
-      this.position[1]
-    ];
-    switch (this.orientation) {
-      case 's':
-        this.position[1] = (this.position[1] + 1) % Grid.sizeY;
-      break;
-      case 'w':
-        this.position[0] = (this.position[0] + 1) % Grid.sizeX;
-      break;
-      case 'n':
-        this.position[1] = (this.position[1] - 1);
-        if (this.position[1] < 0) {
-          this.position[1] = Grid.sizeY + this.position[1];
-        }
-      break;
-      case 'e':
-        this.position[0] = (this.position[0] - 1);
-        if (this.position[0] < 0) {
-          this.position[0] = Grid.sizeX + this.position[0];
-        }
-      break;
-    }
-    if (Grid.thereIsObstacle(this.position[0], this.position[1])) {
-      this.position = formerPosition;
-    }
+  increments: {
+    n: [0, 1],
+    e: [1, 0],
+    s: [0, -1],
+    w: [-1, 0]
   },
   turnRight: function () {
     switch (this.orientation) {
