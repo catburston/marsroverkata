c49c5e766ab0d9974e1087b7a704cd01213153c3
catburston <cat1788@gmail.com>
2014-07-09 11:22:54 +0200

    refactor into classes

--- initialkata/js/tests.js
+++ initialkata/js/tests.js
@@ -1,15 +1,16 @@
-Grid.init(100, 100);
-Rover.init(0, 0, 'n');
-Rover.move('ffrff');
-console.log('Is in 2,2:' + (Rover.position[0] === 2 && Rover.position[1] === 2));
 
-Grid.init(2, 2);
-Rover.init(0, 0, 'n');
-Rover.move('bblbb');
-console.log('Is in 0,0:' + (Rover.position[0] === 0 && Rover.position[1] === 0));
+var grid = new Grid(100, 100);
+var rover = new Rover(0, 0, 'n', grid);
+rover.move('ffrff');
+console.log('Is in 2,2:' + (rover.position[0] === 2 && rover.position[1] === 2));
 
-Grid.init(100, 100);
-Rover.init(0, 0, 'n');
-Grid.setObstacles([[2, 2]]);
-Rover.move('ffrff');
-console.log('Is in 1,2:' + (Rover.position[0] === 1 && Rover.position[1] === 2));
+grid = new Grid(2, 2);
+rover = new Rover(0, 0, 'n', grid);
+rover.move('bblbb');
+console.log('Is in 0,0:' + (rover.position[0] === 0 && rover.position[1] === 0));
+
+grid = new Grid(100, 100);
+rover = new Rover(0, 0, 'n', grid);
+grid.setObstacles([[2, 2]]);
+rover.move('ffrff');
+console.log('Is in 1,2:' + (rover.position[0] === 1 && rover.position[1] === 2));
